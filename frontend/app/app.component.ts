50781a98ff5d6125ce4150e4d911942ce6d13d5e
paullessing <paul@paullessing.com>
2018-08-04 18:01:58 +0100

    Set up Angular

---
+++ frontend/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  constructor(
+    @Inject(DOCUMENT) private document
+  ) {}
+
+  public toggle(): void {
+    this.document.body.classList.toggle('night');
+  }
+}
