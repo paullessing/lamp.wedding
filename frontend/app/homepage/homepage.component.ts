30593d5d6b1137bfe43053d09b992d440c06dea8
paullessing <paul@paullessing.com>
2018-08-12 17:14:23 +0100

    WIP working multi step, STD above ILL

--- frontend/app/homepage/homepage.component.ts
+++ frontend/app/homepage/homepage.component.ts
@@ -41,55 +41,44 @@ import { animate, group, keyframes, query, state, style, transition, trigger } f
   styleUrls: ['./homepage.component.scss'],
   animations: [
     trigger('fadeBackground', [
-      state('night', style({ backgroundColor: '#001d77' })),
+      state('*', style({ backgroundColor: '#001d77' })),
+      state('day', style({ backgroundColor: 'white' })),
       transition('day => night', animate('2s')),
     ]),
-    trigger('fadeLighthouse', [
-      state('title, details', style({ display: 'none' })),
-      transition('night => title', [
-        style({ display: 'block', opacity: 1 }),
-        animate('6s', keyframes([
-          style({ opacity: 1, offset: 0.66 }),
-          style({ opacity: 0, offset: 1 }),
-        ])),
-      ]),
-    ]),
-    trigger('fadeTitle', [
+    trigger('titleFadeIn', [
       state('*', style({ display: 'block' })),
       state('night, title, details', style({ display: 'block', opacity: 1 })),
       transition('day => night', [
         style({ display: 'block', opacity: 0 }),
         animate('2s', style({ display: 'block', opacity: 1 })),
-      //   // animate('6s',
-      //   //   keyframes([
-      //   //   style({ opacity: 0, offset: 0.66 }),
-      //   //   style({ opacity: 1, offset: 1 }),
-      //   // ])),
       ]),
     ]),
-
-    trigger('fadeHeader', [
-      state('details', style({ top: '23vh' })),
+    trigger('lighthouseFadeAway', [
+      state('title, details', style({ display: 'none' })),
       transition('night => title', [
-        style({ top: '*' }),
-        animate('4.5s', style({ top: '*' })),
-        animate('1.5s ease-in-out', style({ top: '23vh' })),
+        style({ display: 'block', opacity: 1 }),
+        animate('2s', style({ opacity: 0, offset: 1 }))
       ]),
     ]),
 
-    trigger('growTitle', [
-      state('title, details', style({ fontSize: '7rem' })),
-      transition('* => title', [
-        style({ fontSize: '*' }),
-        animate('5.5s', style({ fontSize: '*' })),
-        animate('1.5s', style({ fontSize: '7rem' })),
+    trigger('headerMoveToTop', [
+      state('title, details', style({ top: '23vh', fontSize: '1.2rem' })),
+      transition('night => title', [
+        animate('1.5s 1.5s ease-in-out'),
       ]),
     ]),
 
-    trigger('fadeDetails', [
+    trigger('detailsFadeIn', [
       state('*', style({ display: 'none' })),
       state('details', style({ display: 'block', opacity: 1 })),
       transition('title => details', [
+        style({ display: 'block', opacity: 0 }),
+        animate('1.5s ease-in-out', style({ opacity: 1 })),
+      ]),
+    ]),
+
+    trigger('signoffFadeIn', [
+      transition(':enter', [
         style({ display: 'block', opacity: 0 }),
         animate('1.5s ease-in-out', style({ opacity: 1 })),
       ]),
@@ -120,25 +109,21 @@ import { animate, group, keyframes, query, state, style, transition, trigger } f
 export class HomepageComponent {
 
   public animationState: 'day' | 'night' | 'title' | 'details';
+  public showSignoff = false;
 
   constructor(
   ) {
     this.animationState = 'day';
   }
 
-  public goToNight(): void {
+  public start(): void {
+    if (this.animationState !== 'day') {
+      return;
+    }
     this.animationState = 'night';
+    setTimeout(() => this.animationState = 'title', 6000);
+    setTimeout(() => this.animationState = 'details', 9000);
+    setTimeout(() => this.showSignoff = true, 11000);
   }
 
-  public showTitle(): void {
-    if (this.animationState === 'night') {
-      this.animationState = 'title';
-    }
-  }
-
-  public showDetails(): void {
-    if (this.animationState === 'title') {
-      this.animationState = 'details';
-    }
-  }
 }
