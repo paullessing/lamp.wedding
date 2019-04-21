a0462122f77ad51b17976e165dc7f516d63dbef7
paul.lessing <paul@paullessing.com>
2019-04-21 13:40:04 +0200

    Serve binary files

--- frontend.ts
+++ frontend.ts
@@ -1,28 +1,35 @@
-import { pathExists, readFile, stat } from 'fs-extra';
+import { pathExists, readdir, readFile, stat } from 'fs-extra';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import path from 'path';
 import { lookup } from 'mime-types';
 // @ts-ignore
 import StaticFileHandler from 'serverless-aws-static-file-handler';
 
-export const serveStatic: APIGatewayProxyHandler = async (event, _context) => {
+// TODO this is specific to the assets directory. It should work differently for the other files in static
+const clientFilesPath = './static/assets';
+const fileHandler = new StaticFileHandler(clientFilesPath);
+
+export const serveStatic: APIGatewayProxyHandler = async (event, context) => {
   const requestedFilePath = path.join('./static', event.path);
 
   const filePath = (await pathExists(requestedFilePath)) && (await stat(requestedFilePath)).isFile() ? requestedFilePath : './static/index.html';
 
   console.log(event.path, requestedFilePath, filePath);
+  console.log(await readdir(__dirname));
 
   if (event.path.indexOf('/assets') === 0) {
-    const clientFilesPath = path.join(__dirname, './static');
-    console.log('Looks like a thing', JSON.stringify(event, null, '  '));
-    const response = await new StaticFileHandler(clientFilesPath).get(event, _context);
-    console.log('r1', response);
-    return {
-      statusCode: 200,
-      headers: { 'Content-Type': StaticFileHandler.getMimeType(event.path) },
-      body: response.toString(),
-      isBase64Encoded: true,
-    };
+    console.log('Looks like a thing', clientFilesPath, JSON.stringify(event, null, '  '));
+
+    return fileHandler.get(event, context)
+
+    // return await new StaticFileHandler(clientFilesPath).get(event, _context);
+    // console.log('r1', response);
+    // return {
+    //   statusCode: 200,
+    //   headers: { 'Content-Type': StaticFileHandler.getMimeType(event.path) },
+    //   body: response.toString(),
+    //   isBase64Encoded: true,
+    // };
   }
 
 
