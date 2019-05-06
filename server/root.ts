25efe834ac42f916fe78331fdda081e4e2a4b94c
paullessing <paul@paullessing.com>
2019-05-06 12:28:49 +0100

    Add RSVP endpoints

--- server/root.ts
+++ server/root.ts
@@ -6,6 +6,11 @@ import { serveStatic } from './static.handler';
 
 const guestsPutAll = createRequestHandler(guests.putAll);
 const searchGuestByName = createRequestHandler(guests.searchGuestByName);
+const getGuestById = createRequestHandler(guests.getById);
+const getResponseData = createRequestHandler(guests.getResponseData);
+// const putRsvp = createRequestHandler(rsvp.putRsvp);
+
+
 const logView = createRequestHandler(views.logView);
 const sendTestEmail = createRequestHandler(email.sendTest);
 const sendSaveTheDate = createRequestHandler(email.sendSaveTheDate);
@@ -14,6 +19,8 @@ const sendSaveTheDate2 = createRequestHandler(email.sendSaveTheDate2);
 export {
   guestsPutAll,
   searchGuestByName,
+  getGuestById,
+  getResponseData,
   logView,
   sendTestEmail,
   sendSaveTheDate,
