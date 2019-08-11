fab144d945564b2fc7a69b8304c588a1bc5fca02
paullessing <paul@paullessing.com>
2019-08-11 09:33:12 +0100

    Update Picnic Times

--- frontend/app/location/location.component.ts
+++ frontend/app/location/location.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-location',
   templateUrl: './location.component.html',
   styleUrls: ['./location.component.scss']
 })
-export class LocationComponent implements OnInit {
+export class LocationComponent implements AfterViewInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit() {
+  public ngAfterViewInit(): void {
+    if (this.route.snapshot.fragment === 'transport') {
+      document.getElementById('transport-title').scrollIntoView({ block: 'start', behavior: 'smooth' });
+    }
   }
 
 }
